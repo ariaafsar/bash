dc7251c2dc0aa47051ffd8e40fb8a3db003f8a19
aria <aria.afsar@gmail.com>
2024-11-04 16:19:12 +0330

    added a bash script that installs a lot of nesasery app for linux ubuntu

---
+++ bash_scripts/install_apps.sh
@@ -0,0 +1,32 @@
+#!bin/bash
+
+sudo echo "updating system"
+sudo apt update
+
+sudo echo "upgrading"
+sudo apt upgrade
+
+sudo apt update
+
+sudo echo "installing terminator"
+sudo apt install terminator
+
+sudo echo "installing VScode"
+sudo snap install code --classic
+
+sudo echo "installing git"
+sudo apt install git
+
+sudo echo "installing net tools"
+sudo apt install net-tools
+
+sudo echo "installng chromium"
+sudo apt-get update
+sudo apt-get install chromium-browser
+
+sudo echo "installiing vim"
+sudo apt install vim
+
+sudo apt upgrade
+
+sudo echo "all apps installed"
