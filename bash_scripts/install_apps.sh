917428f340e128854250e3a4ebb23f9ecf6c3cfd
aria <aria.afsar@gmail.com>
2024-11-06 03:00:13 +0330

    add docker to install_apps

--- bash_scripts/install_apps.sh
+++ bash_scripts/install_apps.sh
@@ -20,12 +20,27 @@ sudo apt install git
 sudo echo "installing net tools"
 sudo apt install net-tools
 
-sudo echo "installng chromium"
+sudo echo "installing chromium"
 sudo apt-get update
 sudo apt-get install chromium-browser
 
-sudo echo "installiing vim"
+sudo echo "installing vim"
 sudo apt install vim
+
+sudo echo "installing lastest version of docker"
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+#sudo echo "installing tor browser"
 
 sudo apt upgrade
 
