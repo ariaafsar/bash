a06297b87a2283acc8a95048d9a46bfd01638f6a
aria <aria.afsar@gmail.com>
2024-12-11 17:32:28 +0330

    updated 403online dns

--- bash_scripts/dns/403online_dns.sh
+++ bash_scripts/dns/403online_dns.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 
-sudo echo "#this is 403online DNS"
-sudo echo "nameserver 10.202.10.202" >> /etc/resolv.conf
+sudo echo "this is 403online DNS"
+sudo echo "nameserver 10.202.10.202" > /etc/resolv.conf
 sudo echo "nameserver 10.202.10.102" >> /etc/resolv.conf
 
 cat /etc/resolv.conf
